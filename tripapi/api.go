18161eb06a387c015c27cd31ae0187dc2e4bc32d
hibs <hibs@circuitco.de>
2018-04-22 12:47:14 +0000

    automatically uncache files after get check

--- tripapi/api.go
+++ tripapi/api.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"io/ioutil"
+//	"io/ioutil"
 	"net/http"
 	"encoding/json"
 )
@@ -15,16 +15,27 @@ const baseUrl = "http://tripbot.tripsit.me/"
 var endpoints = map[string]string {
 	"alldrugs": "api/tripsit/getAllDrugs",
 }
+const cacheFile = "caches/%s.json"
 
 type FileWriter struct {
 	reader io.Reader
 	cacheName string
+	file *os.File
+	fileOpened bool
 }
 
 func (fw *FileWriter) Read(out []byte) (i int, e error) {
 	i, e = fw.reader.Read(out)
 	if i > 0 {
-		ioutil.WriteFile("caches/"+fw.cacheName+".json", out[:i], 0600)
+		if fw.fileOpened == false {
+			fname := fmt.Sprintf(cacheFile, fw.cacheName)
+			// This probably won't fail in an interesting way.
+			os.Remove(fname)
+			var err error
+			fw.file, err = os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+			fw.fileOpened = err == nil
+		}
+		fw.file.Write(out[:i])
 	}
 	return
 }
@@ -33,35 +44,81 @@ func (fw *FileWriter) Read(out []byte) (i int, e error) {
 var cache = map[string]map[string]interface{}{}
 
 func ensureCacheDir() {
-	err := os.Mkdir("caches", 0600)
+	err := os.Mkdir("caches", 0700)
 	if err != nil && !os.IsExist(err) {
 		fmt.Println(err)
 		panic("Cannot ensure cache directory - are your permissions ok? ")
 	}
 }
 
-func refreshCaches() error {
-	ensureCacheDir()
+func refreshCache(name, url string) error {
 	var client = &http.Client{Timeout: 10 * time.Second}
+	r, err := client.Get(baseUrl+url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	var out map[string]interface{}
+	fwriter := &FileWriter{reader:r.Body, cacheName: name}
+	defer fwriter.file.Close()
+	out, err = decode(fwriter)
+	if err != nil {
+		return err
+	}
+	cache[name] = out
+	return nil
+}
+
+func decode(reader io.Reader) (out map[string]interface{}, err error) {
+	err = json.NewDecoder(reader).Decode(&out)
+	return out, err
+}
+
+func refreshCaches() error {
 	for name, url := range endpoints {
-		r, err := client.Get(baseUrl+url)
-		if err != nil {
-			return err
-		}
-		defer r.Body.Close()
-		var out map[string]interface{}
-		err = json.NewDecoder(&FileWriter{reader: r.Body, cacheName: name}).Decode(&out)
-		if err != nil { return err }
-		cache[name] = out
+		refreshCache(name, url)
 	}
 	fmt.Printf("Hey, I found %d caches\r\n", len(cache))
 	return nil
 }
 
-func Get() string {
-	err := refreshCaches()
+func loadFromFile(fname, name string) (err error) {
+	file, err := os.Open(fname)
 	if err != nil {
-		fmt.Printf("Failed to fetch drugs: %s", err)
+		return err
+	}
+	out, err := decode(file)
+	cache[name] = out
+	return
+}
+
+func checkCaches() error {
+	for name := range endpoints {
+		// Check RAM, file before triggering refresh
+		if cache[name] != nil {
+//			fmt.Printf("Found %s in cache\n", name)
+			continue
+		}
+		fname := fmt.Sprintf(cacheFile, name)
+		_, err := os.Stat(fname)
+		if err == nil {
+//			fmt.Printf("Found %s in file cache\n", name)
+			err = loadFromFile(fname, name)
+		}
+		if err != nil {
+			// Blocking cache refresh, no cache available
+//			fmt.Printf("Couldn't find or load %s, refreshing all\n", name)
+			refreshCaches()
+			break
+		}
+	}
+	return nil
+}
+
+func Get() string {
+	err := checkCaches()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to fetch drugs: %s", err))
 	}
 	return "hey"
 }
